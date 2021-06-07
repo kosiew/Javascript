e2639b628f65a404e5ab8fec7c40d658f59a81a2
kosiew <kosiew@gmail.com>
2021-06-07 09:53:51 +0800

    test: initial files to start with qunit

---
+++ tests.js
@@ -0,0 +1,12 @@
+const { test } = QUnit;
+
+QUnit.module( "Group A" );
+
+const dinos = Dinos().getDinos();
+
+test( "basic test example", assert => {
+  assert.true( true, "this is fine" );
+});
+test( "basic test example 2", assert => {
+  assert.true( true, "this is also fine" );
+});
